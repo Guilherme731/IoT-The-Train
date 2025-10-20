494fe5c8af4af2841f36ef5a5b9b15e4ccec47ab
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-10-20 10:05:41 -0300

    Feito a conexão com o broker

--- S2/S2.ino
+++ S2/S2.ino
@@ -1,20 +1,47 @@
  #include <WiFi.h>
+#include <PubSubClient.h>
+
+WiFiClient client;
+PubSubClient mqtt(client);
 
 const String SSID = "FIESC_IOT_EDU";
 const String PASS = "8120gv08";
+
+
+//constantes p/ broker
+const String URL   = "test.mosquitto.org";
+const int PORT     = 1883;
+const String USR   = "";
+const String broker_PASS  = "";
+const String Topic = "DSM1";
 
  void setup() {
   Serial.begin(115200);
   Serial.println("Conectando ao WiFi");
   WiFi.begin(SSID, PASS);
-  While(WiFi.status() != WL_CONNECTED){
+  while(WiFi.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
   }
-  Serial.println("\nConectado com sucesso!");
+  Serial.print0ln("\nConectado com sucesso!");
+  Serial.println("Conectando ao Broker");
+  mqtt.setServer(URL.c_str(),PORT);
+  while(!mqtt.connected()){
+    String ID = "S2_";
+    ID += String(random(0xffff),HEX); //cria a parte aleat.
+    mqtt.connect(ID.c_str(),USR.c_str(),broker_PASS.c_str());
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado ao broker com sucesso !");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  String mensagem = "Gabriel: ";
+  mensagem += "Olá";
+
+  mqtt.publish(Topic.c_str(),mensagem.c_str());
+  mqtt.loop();
+  delay(1000);
 
 }
